987cf175a92a9874ae71c9c873c80f7feccf8b1f
H.K. An <cohny233@gmail.com>
2025-05-04 22:10:46 +0900

    upgrade download-artifact version

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
-  <HelloWorld msg="Hello World!" />
-  <HelloWorld msg="Testing GitHub Action CI250504_1" />
+  <HelloWorld msg="Welcome to ND's happy world!" />
 </template>
 
 <style scoped>
